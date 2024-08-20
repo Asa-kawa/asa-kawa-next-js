d701265eb16f2dab06032befb89d6afe578c8f64
Asa-kawa <kh7672ak@gmail.com>
2024-08-20 19:56:43 +0900

    asd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ export default function Home() {
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          <code className={styles.code}>src/app/page.tsx</code><br/>
+          <span>BASE_PATH: {process.env.NEXT_PUBLIC_BASE_PATH}</span>
         </p>
         <div>
           <a
