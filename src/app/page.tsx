fdd11095e7e7c98b2a52fa7a82194a4d35a01643
Asa-kawa <kh7672ak@gmail.com>
2024-08-23 10:23:48 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,12 @@ export default function Home() {
   return (
     <div>
       <p>next-js</p>
+      <button
+        onClick={() => {
+          alert(0);
+        }}>
+        Asahi
+      </button>
     </div>
   );
 }
